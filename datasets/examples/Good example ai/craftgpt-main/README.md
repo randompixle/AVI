9878b6c3c7b4176325e7255623ddb86329832e85
randompixle <182171197+randompixle@users.noreply.github.com>
2026-01-29 19:20:58 -0500

    Add datasets via Git LFS

---
+++ datasets/examples/Good example ai/craftgpt-main/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:803f0ea78e6f7a55abe00c71a96e10d2912658b248d978625360d420c6a9390b
+size 2515
---
+++ datasets/examples/Good example ai/craftgpt-main/emulator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:48442ed96519d91d515258b46d2603d260c94bb8f11df689fb1223678d1e7804
+size 22427
